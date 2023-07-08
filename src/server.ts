902f05d823b1c5e3407f13a2a74b2a687e962f4b
Felipe Sartorato <felipesartoratoalfano@gmail.com>
2023-07-08 18:56:15 -0300

    base criada

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+import mustache from "mustache-express"
+import path from "path"
+import mainRoutes from "./routes/index"
 
+dotenv.config()
+
+const server = express()
+
+//Configurando template engine
+server.set("view engine", "mustache")
+server.set("views", path.join(__dirname, "views"))
+server.engine("mustache", mustache())
+
+//Configurando páginas estáticas (public)
+server.use(express.static(path.join(__dirname, "../public")))
+
+//Rotas
+server.listen(process.env.PORT)
+server.use(mainRoutes)
+
+//Rota erro 404
+server.use((req, res) => {
+    res.send("Página não encontrada!")
+})
